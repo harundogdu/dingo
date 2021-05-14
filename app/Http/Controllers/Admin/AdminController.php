095c1b700c74bc3745786d83c1336cfbcfabf9a3
Harun Doğdu <Harundogdu06@gmail.com>
2021-05-15 00:51:26 +0300

    We divided the admin theme into files.

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -14,7 +14,7 @@ class AdminController extends Controller
      */
     public function index()
     {
-        return view('admin.layouts.master');
+        return view('admin.index');
     }
 
     /**
